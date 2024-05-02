5365cf58a7809c00a96fce2efb6c46f636b7fd5f
TianYi0217 <33307265+TianYi0217@users.noreply.github.com>
2024-05-02 11:31:39 +0800

    update gpt-4-tubro delete usual models

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -49,7 +49,7 @@ export interface Folder {
   color?: string;
 }
 
-export type ModelOptions = 'gpt-4' | 'gpt-4-32k' | 'gpt-4-1106-preview' | 
+export type ModelOptions = 'gpt-4-tubro' | 'gpt-4' | 'gpt-4-32k' | 'gpt-4-1106-preview' | 
 'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k' | 'gpt-3.5-turbo-1106' | 'bing' | 
 'claude-2' | 'gemini-pro' | 'google-palm' | 'gpt-4-all' | 'gpt-4-gizmo' | 
 'net-gpt-4' | 'gpt-4-0125-preview' | 'net-gpt-3.5-turbo' |
